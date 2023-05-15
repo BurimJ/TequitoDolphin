4ed4029f62412b7663a34174e62b5aed934e76ac
BurimJ <124050089+BurimJ@users.noreply.github.com>
2023-05-15 23:25:36 +0200

    add stuff

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import { initViews } from "./view-router.js";
 window.addEventListener("load", initApp);
 
 function initApp() {
-    console.log("app.js is running 🎉");
-    initViews();
+  console.log("app.js is running 🎉");
+  initViews();
+  document.querySelector("#btn__add-member").addEventListener("click", showAddMemberDialog);
+  document.querySelector("#btn__add-competitor").addEventListener("click", showAddCompetitorDialog);
+  document.querySelector("#form-for-member").addEventListener("submit", addMemberClicked);
+  document.querySelector("#form-for-competitor").addEventListener("submit", addCompetitorClicked);
+  document.querySelector("#form-update-member").addEventListener("submit", updateMemberClicked);
+  document.querySelector("#form-update-competitor").addEventListener("submit", updateCompetitorClicked);
+  document.querySelector("#form-delete-post").addEventListener("submit", deletePostClicked);
 }
---
+++ create-competitor.js
@@ -0,0 +1,48 @@
+"use strict";
+
+console.log("yo");
+let modalOpen = false;
+
+function showAddCompetitorDialog() {
+  modalOpen = true;
+  document.querySelector("#dialog-for-competitor").showModal();
+}
+
+function addCompetitorClicked(event) {
+  event.preventDefault();
+  console.log("add Competitor clicked");
+
+  const form = document.querySelector("#form-for-competitor");
+  if (event.submitter.innerHTML === "Close") {
+    document.querySelector("#dialog-for-competitor").close();
+    return;
+  }
+  addCompetitor();
+  form.reset();
+  document.querySelector("#dialog-for-competitor").close();
+  modalOpen = false;
+}
+
+async function addCompetitor() {
+  const elements = document.querySelector("#form-for-competitor").elements;
+
+  const competitor = {
+    firstName: elements.firstName.value,
+    lastName: elements.lastName.value,
+    myTeam: elements.myTeam.value,
+    myPayment: elements.myPayment.value,
+    myDistance: elements.myDistance.value,
+  };
+
+  const json = JSON.stringify(competitor);
+  const response = await fetch(`${endpoint}/competitors.json`, {
+    method: "POST",
+    body: json,
+  });
+
+  if (response.ok) {
+    console.log("new competitor added!");
+    showCompetitorGrid();
+  }
+  console.log(competitor);
+}
