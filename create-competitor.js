3c4cb4b293c71820b9cb1866a2a26afaa99eb15a
BurimJ <124050089+BurimJ@users.noreply.github.com>
2023-05-20 23:14:12 +0200

    mem+comp

--- create-competitor.js
+++ create-competitor.js
@@ -1,21 +1,82 @@
 "use strict";
 
-console.log("yo");
+window.addEventListener("load", start);
 
-function showAddCompetitorDialog() {
-  modalOpen = true;
-  document.querySelector("#dialog-for-competitor").showModal();
+// end point
+
+const endpoint = "https://semesterprojekt-790e8-default-rtdb.europe-west1.firebasedatabase.app";
+
+function prepareData(dataObject) {
+  const array = [];
+  for (const key in dataObject) {
+    const object = dataObject[key];
+    object.id = key;
+    array.push(object);
+  }
+  return array;
+}
+
+function start() {
+  showCompetitorGrid();
+
+  document.querySelector("#btn__add-competitor").addEventListener("click", showAddCompetitorDialog);
+  document.querySelector("#form-for-competitor").addEventListener("submit", addCompetitorClicked);
+  // document.querySelector("#form-update-competitor").addEventListener("submit", updateCompetitorClicked);
+}
+
+let competitors;
+
+async function getCompetitors() {
+  const response = await fetch(`${endpoint}/competitors.json`);
+  const data = await response.json();
+  const competitors = prepareData(data);
+  return competitors;
+}
+
+function showCompetitor(competitor) {
+  const html = /* html */ `
+        <article class="competitors__item">
+            <h3>First Name: ${competitors.firstName}<h3>
+            <h3>Last Name: ${competitors.class}<h3>
+            <h3>Age: ${competitors.age}<h3>
+            <h3>Team: ${competitors.myTeam}<h3>
+            <h3>Payment: ${competitors.myPayment}<h3>
+            <h3>Distance: ${competitors.myDistance}<h3>
+            <button id="btn-update-competitor" class="btn__style">Update competitor</button>
+            <button id="btn-delete-competitor" class="btn__style">Delete competitor</button>
+        </article>
+    `;
+
+  document.querySelector("#competitors").insertAdjacentHTML("beforeend", html);
+
+  //event listeners to btns
+  document.querySelector("#competitors article:last-child #btn-update-competitor").addEventListener("click", () => updateClicked(competitor));
+  document.querySelector("#competitors article:last-child #btn-delete-competitor").addEventListener("click", () => deleteClicked(competitor));
+}
+
+function showcompetitors(competitors) {
+  document.querySelector("#competitors").innerHTML = "";
+
+  for (const competitor of competitors) {
+    showCompetitor(competitor);
+  }
+}
+
+async function showCompetitorGrid() {
+  competitors = await getCompetitors();
+  showCompetitor(competitors);
 }
 
 function addCompetitorClicked(event) {
   event.preventDefault();
   console.log("add Competitor clicked");
-
   const form = document.querySelector("#form-for-competitor");
+
   if (event.submitter.innerHTML === "Close") {
     document.querySelector("#dialog-for-competitor").close();
     return;
   }
+
   addCompetitor();
   form.reset();
   document.querySelector("#dialog-for-competitor").close();
@@ -32,16 +93,7 @@ async function addCompetitor() {
     myPayment: elements.myPayment.value,
     myDistance: elements.myDistance.value,
   };
-
-  const json = JSON.stringify(competitor);
-  const response = await fetch(`${endpoint}/competitors.json`, {
-    method: "POST",
-    body: json,
-  });
-
-  if (response.ok) {
-    console.log("new competitor added!");
-    showCompetitorGrid();
-  }
-  console.log(competitor);
+}
+function showAddCompetitorDialog() {
+  document.querySelector("#dialog-for-competitor").showModal();
 }
--- create-member.js
+++ create-member.js
@@ -1,22 +1,82 @@
 "use strict";
 
-console.log("yo");
-let modalOpen = false;
+window.addEventListener("load", start);
 
-function showAddMemberDialog() {
-  modalOpen = true;
-  document.querySelector("#dialog-for-member").showModal();
+// end point
+
+const endpoint = "https://semesterprojekt-790e8-default-rtdb.europe-west1.firebasedatabase.app";
+
+function prepareData(dataObject) {
+  const array = [];
+  for (const key in dataObject) {
+    const object = dataObject[key];
+    object.id = key;
+    array.push(object);
+  }
+  return array;
+}
+
+function start() {
+  showMemberGrid();
+
+  document.querySelector("#btn__add-member").addEventListener("click", showAddMemberDialog);
+  document.querySelector("#form-for-member").addEventListener("submit", addMemberClicked);
+  // document.querySelector("#form-update-competitor").addEventListener("submit", updateCompetitorClicked);
+}
+
+let members;
+
+async function getMembers() {
+  const response = await fetch(`${endpoint}/members.json`);
+  const data = await response.json();
+  const members = prepareData(data);
+  return members;
+}
+
+function showMember(member) {
+  const html = /* html */ `
+        <article class="competitors__item">
+            <h3>First Name: ${members.firstName}<h3>
+            <h3>Last Name: ${members.class}<h3>
+            <h3>Age: ${members.age}<h3>
+            <h3>Team: ${members.myTeam}<h3>
+            <h3>Payment: ${members.myPayment}<h3>
+            <h3>Distance: ${members.myDistance}<h3>
+            <button id="btn-update-member" class="btn__style">Update Member</button>
+            <button id="btn-delete-member" class="btn__style">Delete Member</button>
+        </article>
+    `;
+
+  document.querySelector("#members").insertAdjacentHTML("beforeend", html);
+
+  //event listeners to btns
+  document.querySelector("#members article:last-child #btn-update-member").addEventListener("click", () => updateClicked(member));
+  document.querySelector("#members article:last-child #btn-delete-member").addEventListener("click", () => deleteClicked(members));
+}
+
+function showmembers(members) {
+  document.querySelector("#members").innerHTML = "";
+
+  for (const member of members) {
+    showMember(member);
+  }
+}
+
+async function showMemberGrid() {
+  members = await getMembers();
+  showMember(members);
 }
 
 function addMemberClicked(event) {
   event.preventDefault();
   console.log("add member clicked");
-
   const form = document.querySelector("#form-for-member");
+
   if (event.submitter.innerHTML === "Close") {
     document.querySelector("#dialog-for-member").close();
     return;
   }
+
   addMember();
   form.reset();
   document.querySelector("#dialog-for-member").close();
@@ -26,23 +86,14 @@ function addMemberClicked(event) {
 async function addMember() {
   const elements = document.querySelector("#form-for-member").elements;
 
-  const member = {
+  const competitor = {
     firstName: elements.firstName.value,
     lastName: elements.lastName.value,
     myTeam: elements.myTeam.value,
     myPayment: elements.myPayment.value,
     myDistance: elements.myDistance.value,
   };
-
-  const json = JSON.stringify(member);
-  const response = await fetch(`${endpoint}/members.json`, {
-    method: "POST",
-    body: json,
-  });
-
-  if (response.ok) {
-    console.log("new Member added!");
-    showMemberGrid();
-  }
-  console.log(member);
+}
+function showAddMemberDialog() {
+  document.querySelector("#dialog-for-member").showModal();
 }
