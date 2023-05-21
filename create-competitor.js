7242a73a727cd8af82cb9ab416352060a5cacfa2
BurimJ <124050089+BurimJ@users.noreply.github.com>
2023-05-21 02:31:04 +0200

    comp create , update and delete done

--- create-competitor.js
+++ create-competitor.js
@@ -21,7 +21,8 @@ function start() {
 
   document.querySelector("#btn__add-competitor").addEventListener("click", showAddCompetitorDialog);
   document.querySelector("#form-for-competitor").addEventListener("submit", addCompetitorClicked);
-  // document.querySelector("#form-update-competitor").addEventListener("submit", updateCompetitorClicked);
+  document.querySelector("#dialog-delete-competitor").addEventListener("submit", deleteClicked);
+  document.querySelector("#form-update-competitor").addEventListener("submit", updateClicked);
 }
 
 let competitors;
@@ -37,7 +38,7 @@ function showCompetitor(competitor) {
   const html = /* html */ `
         <article class="competitors__item">
             <p>First Name: ${competitor.firstName}<p>
-            <p>Last Name: ${competitor.class}<p>
+            <p>Last Name: ${competitor.lastName}<p>
             <p>Age: ${competitor.age}<p>
             <p>Team: ${competitor.myTeam}<p>
             <p>Payment: ${competitor.myPayment}<p>
@@ -50,8 +51,8 @@ function showCompetitor(competitor) {
   document.querySelector("#competitors").insertAdjacentHTML("beforeend", html);
 
   //event listeners to btns
-  document.querySelector("#competitors article:last-child #btn-update-competitor").addEventListener("click", () => updateClicked(competitor));
-  document.querySelector("#competitors article:last-child #btn-delete-competitor").addEventListener("click", () => deleteClicked(competitor));
+  document.querySelector("#competitors article:last-child #btn-update-competitor").addEventListener("click", () => updateCompetitorClicked(competitor));
+  document.querySelector("#competitors article:last-child #btn-delete-competitor").addEventListener("click", () => deleteCompetitorClicked(competitor));
 }
 
 function showCompetitors(competitors) {
@@ -73,11 +74,13 @@ async function addCompetitor() {
   const elements = document.querySelector("#form-for-competitor").elements;
 
   const competitor = {
-    firstName: elements.firstName.value,
-    lastName: elements.lastName.value,
-    myTeam: elements.myTeam.value,
-    myPayment: elements.myPayment.value,
-    myDistance: elements.myDistance.value,
+    firstName: elements.competitorFirstName.value,
+    lastName: elements.competitorLastName.value,
+    age: elements.competitorAge.value,
+    //bithdate: elements.competitorBirthdate.value,
+    myTeam: elements.competitorTeam.value,
+    myPayment: elements.competitorPayment.value,
+    myDistance: elements.competitorDistance.value,
   };
   const json = JSON.stringify(competitor);
   const response = await fetch(`${endpoint}/competitors.json`, {
@@ -110,4 +113,86 @@ function addCompetitorClicked(event) {
 
 function showAddCompetitorDialog() {
   document.querySelector("#dialog-for-competitor").showModal();
+}
+
+// delete comp
+
+function deleteCompetitorClicked(competitor) {
+  const deleteCompetitor = document.querySelector("#form-delete-competitor");
+
+  deleteCompetitor.setAttribute("data-id", competitor.id);
+  document.querySelector("#dialog-delete-competitor").showModal();
+}
+
+function deleteClicked(event) {
+  const id = event.target.getAttribute("data-id");
+
+  if (event.submitter.innerHTML === "Yes") {
+    deleteCompetitor(id);
+  }
+
+  document.querySelector("#dialog-delete-competitor").close();
+}
+
+async function deleteCompetitor(id) {
+  const response = await fetch(`${endpoint}/competitors/${id}.json`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    console.log("competitor deleted!");
+    showCompetitorGrid();
+  }
+}
+
+function updateCompetitorClicked(competitor) {
+  const update = document.querySelector("#form-update-competitor");
+  console.log("update", update);
+  update.competitorUpdateFirstName.value = competitor.firstName;
+  update.competitorUpdateLastName.value = competitor.lastName;
+  update.competitorUpdateAge.value = competitor.age;
+  update.competitorUpdateTeam.value = competitor.myTeam;
+  update.competitorUpdatePayment.value = competitor.myPayment;
+  // update.competitorUpdateBirthdate.value = competitors.birthdate;
+  update.competitorUpdateDistance.value = competitor.myDistance;
+
+  update.setAttribute("data-id", competitor.id);
+  document.querySelector("#dialog-update-competitor").showModal();
+}
+
+function updateClicked(event) {
+  event.preventDefault();
+  const form = event.target;
+  const id = form.getAttribute("data-id");
+  if (event.submitter.innerHTML === "Update Competitor") {
+    updateCompetitor(id);
+  }
+  document.querySelector("#dialog-update-competitor").close();
+}
+
+async function updateCompetitor(id) {
+  const elements = document.querySelector("#form-update-competitor").elements;
+  console.log("elements", elements);
+
+  const competitor = {
+    firstName: elements.competitorUpdateFirstName.value,
+    lastName: elements.competitorUpdateLastName.value,
+    age: elements.competitorUpdateAge.value,
+    //bithdate: elements.competitorUpdateBirthdate.value,
+    myTeam: elements.competitorUpdateTeam.value,
+    myPayment: elements.competitorUpdatePayment.value,
+    myDistance: elements.competitorUpdateDistance.value,
+  };
+  console.log("competitor", competitor);
+  const json = JSON.stringify(competitor);
+  const response = await fetch(`${endpoint}/competitors/${id}.json`, {
+    method: "PUT",
+    body: json,
+  });
+
+  if (response.ok) {
+    console.log("competitor updated!");
+    showCompetitorGrid();
+  }
+  console.log(competitor);
 }
